2aa97c990bd8a9c9a433d37372fbd2920b865112
wish9 <wjwee9@gmail.com>
2023-02-06 17:56:45 +0900

    Initial commit

---
+++ src/main/java/com/codestates/section2week4/Coffee/CoffeeService.java
@@ -0,0 +1,20 @@
+package com.codestates.section2week4.Coffee;
+
+public class CoffeeService {
+    private static CoffeeRepository coffeeRepository = new CoffeeRepository();
+
+    public void createCoffee(Coffee coffee) {
+        coffeeRepository.postCoffee(coffee);
+    }
+
+    public Coffee editCoffee(Long coffeeId, String korName, int price) {
+        return coffeeRepository.patchCoffee(coffeeId, korName, price);
+    }
+
+    public Coffee getCoffee(Long coffeeId) {
+        return coffeeRepository.getCoffee(coffeeId);
+    }
+    public void deleteCoffee(Long coffeeId) {
+        coffeeRepository.deleteCoffee(coffeeId);
+    }
+}
---
+++ src/main/java/com/codestates/section2week4/Coffee/CoffeeTest.java
@@ -0,0 +1,27 @@
+package com.codestates.section2week4.Coffee;
+
+public class CoffeeTest {
+    public static void main(String[] args) {
+
+        CoffeeService coffeeService = new CoffeeService();
+
+        Coffee coffee = new Coffee(0L, "바닐라 라떼", "vanilla latte", 5000);
+        coffeeService.createCoffee(coffee);
+
+        if(coffeeService.getCoffee(0L).getKorName().equals(coffee.getKorName())) {
+            System.out.println("바닐라 라떼가 등록되었습니다.");
+        }
+
+        coffeeService.editCoffee(0L, "바닐라 라떼", 3000);
+
+        if(coffeeService.getCoffee(0L).getPrice() == 3000) {
+            System.out.println("바닐라 라떼의 금액이 정상적으로 변경되었습니다.");
+        }
+
+        coffeeService.deleteCoffee(0L);
+
+        if(coffeeService.getCoffee(0L) == null) {
+            System.out.println("바닐라 라떼가 정상적으로 삭제되었습니다.");
+        }
+    }
+}
